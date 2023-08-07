b99dc16238bee7435c48f50020974187127b270e
guigabriel <guigabriel@mailfence.com>
2023-08-07 09:37:55 -0300

    First commit

---
+++ src/main/java/com/ibm/ibmproject/IbmprojectApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.ibmproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbmprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbmprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibm/ibmproject/IbmprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.ibmproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbmprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
